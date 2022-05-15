17ec6e4d181833665facb2f177fa761bfe37c8fb
Your Name <skorokhodov.vs@phystech.edu>
2022-05-15 15:43:03 +0300

    change readme

--- scripts/run.py
+++ scripts/run.py
@@ -9,8 +9,8 @@ def main():
     parser.add_argument("-m", dest="m", default=1, type=float, help="mass of every body")
     parser.add_argument("-b", dest="b", default=1, type=float, help="dissipation coefficient")
     parser.add_argument("-c", dest="c", default=1, type=float, help="stiffness coefficient")
-    parser.add_argument("-t", dest="timedelta", default=0.1, type=float, help="how many seconds in one real second") # or make per iteration?
-    parser.add_argument("-r", dest="r", default=200, type=float, help="radius of circle")
+    parser.add_argument("-t", dest="timedelta", default=1.0, type=float, help="how many seconds in one real second") # or make per iteration?
+    parser.add_argument("-r", dest="r", default=200, type=float, help="radius of the main circle")
     parser.add_argument('--q0', dest="q0", action='store', type=float, help='initial state', nargs=4)
     args = parser.parse_args()
 
